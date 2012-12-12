988ab736bd27f92a4f66985b99febf1b491e0d87
shane-wright <shane.wright@yomdom.com>
2012-12-12 11:57:37 -0500

    Some Comment

--- heroku/Ydu/src/main/java/com/yomdom/ydu/controller/YduController.java
+++ heroku/Ydu/src/main/java/com/yomdom/ydu/controller/YduController.java
@@ -10,7 +10,7 @@ public class YduController extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    resp.getWriter().print("Hello from Caleb!\n");
+    resp.getWriter().print("Hello from Slim Shaney!\n");
   }
 
   public static void main(String[] args) throws Exception{
