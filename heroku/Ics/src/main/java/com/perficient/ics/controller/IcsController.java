5251ff2b746a74b1edcce72606f118c710ab2081
4mnes7y <caleb.eades@yomdom.com>
2012-12-16 17:04:32 -0500

    Established connection to the database

--- heroku/Ics/src/main/java/com/perficient/ics/controller/IcsController.java
+++ heroku/Ics/src/main/java/com/perficient/ics/controller/IcsController.java
@@ -2,12 +2,26 @@ package com.perficient.ics.controller;
 
 import java.io.IOException;
 
+
+
+import java.util.*;
+import java.net.*;
+import java.io.*;
+import java.sql.*;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+
+
+
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.perficient.ics.model.Agent;
 
 @WebServlet(
     name = "IcsController", 
@@ -20,6 +34,18 @@ public class IcsController extends HttpServlet {
     ServletOutputStream out = resp.getOutputStream();
     out.write("{ \"total\":\"1\", \"page\":\"1\", \"records\":\"2\", \"rows\": [ { \"id\":\"1\", \"cell\": [\"1\",\"Time\",\"Time Agent\"] }, { \"id\":\"2\", \"cell\": [\"2\",\"Weather\",\"Weather Agent\"] } ] }".getBytes());
 
+    ArrayList<Agent> agents = null;
+    try {
+      agents = Agent.list();
+    }
+    catch(Exception ex) {
+      ex.printStackTrace();
+    }
+
+   for(Agent agent : agents) {
+      System.out.println("Agent: " + agent.getName() + " - " + agent.getDescription());
+    }
+    
     out.flush();
     out.close();
   }
