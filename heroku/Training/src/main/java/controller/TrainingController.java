2d7e75c78cb965025ae8e124618f408fda193908
seanw7135 <sean.wright@yomdom.com>
2012-12-26 16:03:23 -0500

    Mods

--- heroku/Training/src/main/java/controller/TrainingController.java
+++ heroku/Training/src/main/java/controller/TrainingController.java
@@ -9,6 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.*;
+
+import model.*;
+
 @WebServlet(
   name = "TrainingController", 
   urlPatterns = {"/training"}
@@ -18,10 +22,30 @@ public class TrainingController extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    ServletOutputStream out = resp.getOutputStream();
-    out.write("hello heroku".getBytes());
-    out.flush();
-    out.close();
+
+    Agent.init();
+
+    ArrayList<Agent> agents = new ArrayList<Agent>();
+    agents.add(new Agent(0, "Weather", "My Weather"));
+    agents.add(new Agent(0, "News", "My News"));
+
+    Agent.create(agents);
+
+    agents = null;
+
+    try {
+      agents = Agent.list();
+      String json = Agent.toJsonGrid(agents);
+      ServletOutputStream out = resp.getOutputStream();
+      out.write(json.getBytes());
+      out.flush();
+      out.close();
+    }
+    catch(Exception ex) {
+      ex.printStackTrace();
+    }
+
+
   }
   
 }
