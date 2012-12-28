6b15da4fb3ae711595e56e201e2e4c139e785e80
Brock Wright <brock.wright@perficient.com>
2012-12-28 13:36:37 -0500

    Added model..

---
+++ heroku/Habanero/src/main/java/model/Test.java
@@ -0,0 +1,29 @@
+package model;
+
+
+public class Test {
+
+  private String name:
+  private Integer testId;
+
+  public Test(String name, Integer testId) {
+
+    public void setName(String name) {
+      this.name = name;
+    }  
+
+    public String getName() {
+      return name;
+    }  
+
+    public void setTestId(Integer testId) {
+      this.testId = testId;
+    }  
+
+    public Integer getTestId() {
+      return testId;
+    }
+
+  }
+
+}
