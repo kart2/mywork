dfa84a7a2e87aacef385c1722a1ef3b6707910f9
Brock Wright <brock.wright@perficient.com>
2012-12-31 16:01:37 -0500

    Mods..

--- java/JavaSource/src/main/java/com/perficient/ics/javasource/controller/JavaSourceController.java
+++ java/JavaSource/src/main/java/com/perficient/ics/javasource/controller/JavaSourceController.java
@@ -1,24 +1,62 @@
 package com.perficient.ics.javasource.controller;
 
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
 /**
  * JavaSourceController
  *
  */
 public class JavaSourceController {
   public static void main( String[] args ) {
-    String projectName = args[0];
-    String className   = args[1];
+
+    boolean importValue = false;
+
+    String projectName = "";
+    String importName = "";
+    String className = "";
+
+    ArrayList<String> importArray = new ArrayList<String>();
+
+    try {
+      java.io.BufferedReader stdin = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+      
+    while(importValue == false) {
+      System.out.print("Enter import: ");
+      importName = stdin.readLine();
+      importArray.add(importName);
+
+      if(importName.length() == 0) {
+        importValue = true;
+        break;
+      }
+    }
+
+      System.out.print("Enter project name: ");
+      projectName = stdin.readLine();
+
+      System.out.print("Enter class name: ");
+      className = stdin.readLine();
+    }
+    catch(Exception e) {
+      System.out.println(e);
+    }
 
     StringBuilder sourceBuilder = new StringBuilder();
-    sourceBuilder.append("package com.perficient.ics.")
-                 .append(projectName)
-                 .append(";\n\n");
+    Iterator iter = importArray.iterator();
 
-    sourceBuilder.append("import java.util.*;\n\n");
+    while(iter.hasNext()) {
+
+    sourceBuilder.append("import ")
+    		 .append(iter.next() + ";\n\n");
+    }
+
+    sourceBuilder.append("package com.perficient.ics.")
+                 .append(projectName + ";\n\n");
 
     sourceBuilder.append("public class ")
-                 .append(className)
-                 .append(" {\n");
+                 .append(className + " {\n");
     
     sourceBuilder.append("  public static void main(String[] args) {\n");
     sourceBuilder.append("    System.out.println(\"Java Source Generator\");\n");
