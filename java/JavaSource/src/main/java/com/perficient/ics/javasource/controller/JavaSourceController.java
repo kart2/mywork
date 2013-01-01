bfce40ac9de06986ecd7d27b21ab35c1ce8e9301
seanw7135 <sean.wright@yomdom.com>
2013-01-01 16:48:13 -0500

    Added JSON Support

--- java/JavaSource/src/main/java/com/perficient/ics/javasource/controller/JavaSourceController.java
+++ java/JavaSource/src/main/java/com/perficient/ics/javasource/controller/JavaSourceController.java
@@ -4,12 +4,24 @@ import java.util.*;
 import java.net.*;
 import java.io.*;
 
+import java.io.File;
+import java.io.IOException;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.perficient.ics.javasource.model.*;
+
 public class JavaSourceController {
   public static void main( String[] args ) {
     int length = args.length;
      
     if (length != 1) {
       System.out.println("Usage: JavaSourceController <JSON File>");  
+      System.out.println("JSON file should contain:\n");
+      System.out.println("  {");
+      System.out.println("    \"name\" :  \"myproject\"");
+      System.out.println("  }");
       System.exit(-1);
     }
 
@@ -17,7 +29,16 @@ public class JavaSourceController {
 
     System.out.println("JSON File: " + jsonFileName + "\n");
 
-    ModelCreateController modelCreate = new ModelCreateController();
+    ObjectMapper mapper = new ObjectMapper();
+    Project project = null;
+    try {
+      project = mapper.readValue(new File(jsonFileName), Project.class);
+    }
+    catch(Exception e) {
+      e.printStackTrace();
+    }
+
+    ModelCreateController modelCreate = new ModelCreateController(project);
   }  
 }
 
--- java/JavaSource/src/main/java/com/perficient/ics/javasource/controller/ModelCreateController.java
+++ java/JavaSource/src/main/java/com/perficient/ics/javasource/controller/ModelCreateController.java
@@ -4,30 +4,95 @@ import java.util.*;
 import java.net.*;
 import java.io.*;
 
+import com.perficient.ics.javasource.model.*;
+
 public class ModelCreateController {
-  public ModelCreateController() {
-    String projectName = "test";
-    String className   = "Model";
+  public ModelCreateController(Project project) {
 
     StringBuilder sourceBuilder = new StringBuilder();
 
     // Package
-    sourceBuilder.append("package com.perficient.")
-                 .append(projectName)
-                 .append(";\n\n");
+    sourceBuilder.append("package com.perficient.ics.")
+                 .append(project.getName())
+                 .append(".model;\n\n");
 
 
     // Imports 
     sourceBuilder.append("import java.util.*;");
+    sourceBuilder.append("\n");
     sourceBuilder.append("import java.net.*;");
+    sourceBuilder.append("\n");
     sourceBuilder.append("import java.io.*;");
+    sourceBuilder.append("\n");
     sourceBuilder.append("import java.sql.*;");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("\n");
     
 
     // Class definition
-    sourceBuilder.append("public class ")
-                 .append(className)
-                 .append(" {\n");
+    sourceBuilder.append("public class Model {");
+    sourceBuilder.append("\n");
+
+ 
+    // Declare static variables
+    sourceBuilder.append("  private static final String DB_DRIVER     = \"org.postgresql.Driver\";");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("  private static final String DB_CONNECTION = \"jdbc:postgresql://127.0.0.1:5432/ics\";");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("  private static final String DB_USER       = \"ics\";");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("  private static final String DB_PASSWORD   = \"ics\";");
+    sourceBuilder.append("\n");
+
+    // getConnection method
+    sourceBuilder.append("  public static Connection getDBConnection() {");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("    Connection dbConnection = null;");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("    try {");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("      Class.forName(DB_DRIVER);");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("    }");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("    catch (ClassNotFoundException e) {");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("      System.out.println(e.getMessage());");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("    }");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("    try {");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("      dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER, DB_PASSWORD);");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("      return dbConnection;");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("    }");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("    catch (SQLException e) {");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("      System.out.println(e.getMessage());");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("    }");
+    sourceBuilder.append("\n");
+
+    sourceBuilder.append("    return dbConnection;");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("  }");
+    sourceBuilder.append("\n");
+
+    sourceBuilder.append("  public static java.sql.Timestamp getCurrentTimeStamp() {");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("    java.util.Date today = new java.util.Date();");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("    return new java.sql.Timestamp(today.getTime());");
+    sourceBuilder.append("\n");
+    sourceBuilder.append("  }");
+
+    sourceBuilder.append("\n");
 
     // End Class definition
     sourceBuilder.append("}\n");
