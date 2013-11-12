e43eecb61f6a18091801d5a80a1f715ee148b72b
karthick <regards_uk@yahoo.com>
2013-11-12 11:13:36 -0500

    fssdfs

---
+++ Downloads/phonegap-2.8.1/phonegap/Training/Training/TrainingDemoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TrainingDemoAppDelegate.h
+//  Training
+//
+//  Created by Karthick.Udayakumar on 6/18/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrainingDemoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Downloads/phonegap-2.8.1/phonegap/Training/Training/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Training
+//
+//  Created by Karthick.Udayakumar on 6/18/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TrainingDemoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TrainingDemoAppDelegate class]));
+    }
+}
